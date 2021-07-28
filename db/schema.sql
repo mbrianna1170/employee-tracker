3ce8272f30e00f68e08cecf8aacd2b1e589ac21c
mbrianna1170 <brianna.miller09@yahoo.com>
2021-07-28 13:53:47 -0500

    connected tables'

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
-    salary DECIMAL NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE department (
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,13 +5,13 @@ VALUES
     ('Finance'),
     ('Legal');
 
-INSERT INTO roles (title, salary)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-    ('Content Creator', 50000),
-    ('Sales Lead', 60000),
-    ('Marketing Manager', 70000),
-    ('Software Engineer', 85000),
-    ('Lead Engineer', 95000),
-    ('Accountant', 65000),
-    ('Lawyer', 150000),
-    ('Paralegal', 55000);
+    ('Content Creator', 50000, '1'),
+    ('Sales Lead', 60000, '1'),
+    ('Marketing Manager', 70000, '1'), 
+    ('Software Engineer', 85000, '2'),
+    ('Lead Engineer', 95000, '2'),
+    ('Accountant', 65000, '3'),
+    ('Lawyer', 150000, '4'),
+    ('Paralegal', 55000, '4');
